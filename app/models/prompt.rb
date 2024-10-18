0a939ab604fd742c6b82d201a695f232fa52c25b
papayalabs <papayalabs@gmail.com>
2024-10-18 15:01:26 +0200

    Minimiza all prompt windows

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,4 +1,8 @@
 class Prompt < ApplicationRecord
   scope :priority, -> { order(priority: :asc) }
   belongs_to :prompt_process
+
+  def truncate(string, max)
+    string.length > max ? "#{string[0...max]}..." : string
+  end
 end
