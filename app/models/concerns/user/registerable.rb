c407f8189ca510882772fd88808a9706ccc01be5
papayalabs <papayalabs@gmail.com>
2024-11-02 14:26:40 +0100

    Update assistant in registrable

--- app/models/concerns/user/registerable.rb
+++ app/models/concerns/user/registerable.rb
@@ -8,7 +8,8 @@ module User::Registerable
   private
 
   def create_initial_assistants
-   # assistants.create! name: "Ollama Llama 3.2", model: "llama3.2", images: true
-    assistants.create! name: "Gemini", model: "gemini-1.5-flash", images: true
+    assistants.create! name: "Ollama Llama 3.2", model: "llama3.2", api_url: "http://localhost:11434", images: false
+    assistants.create! name: "Gemini Flahs 1.5", model: "gemini-1.5-flash", api_url: "https://api.gemini.com/v1/", images: true
+    assistants.create! name: "Claude Haiku 3.0", model: "claude-3-haiku-20240307", api_url: "https://api.anthropic.com/", images: true
   end
 end
