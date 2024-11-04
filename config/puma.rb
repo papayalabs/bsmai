4fe43595fa2faefdea7f9c385841c23e3c6b62a3
papayalabs <papayalabs@gmail.com>
2024-11-04 09:46:27 +0100

    CSS Fix

--- config/puma.rb
+++ config/puma.rb
@@ -8,42 +8,30 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") {
-  ENV.fetch("RAILS_ENV", "development") == "development" ? 2 : 5
-}
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+threads 5,5
 
 # Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require 'puma/daemon'
-  require "concurrent-ruby"
-  daemonize
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-  shared_dir = "/var/www/bsmai/shared"
+workers 2
+
 # Logging
-  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log"
-end
+shared_dir = "/var/www/bsmai/shared"
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log"
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 9838 }
+port 9838
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment "production"
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+#pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-if ENV.fetch("RUN_SOLID_QUEUE_IN_PUMA") { false }
-  plugin :solid_queue
-end
-
+plugin :solid_queue
 #plugin :tailwindcss if ENV.fetch("RAILS_ENV", "development") == "development"
